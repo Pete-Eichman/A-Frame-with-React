530bc4ca57bb39012e90925d052b1d4dcec6aa99
Pete Eichman <peichman14@gmail.com>
2017-03-13 23:38:30 -0400

    add more shapes

--- src/VRScene.js
+++ src/VRScene.js
@@ -10,6 +10,9 @@ class VRScene extends React.Component {
         <Entity geometry={{primitive: 'box', width: 2, height: 2}} material="color: red" position={[0, 0, -5]}/>
         <Entity geometry={{primitive: 'sphere', radius: .25}} material="color: blue" position={[1, 2, -3]}/>
         <Entity geometry={{primitive: 'plane', width: 2, height: 2}} material="color: green" position={[0, 0, -1]} rotation={[-90, 0, 0]}/>
+        <Entity geometry={{primitive: 'box', width: 2, height: 2}} material="color: red" position={[3, 4, 5]}/>
+        <Entity geometry={{primitive: 'sphere', radius: .25}} material="color: blue" position={[-1, -2, 3]}/>
+        <Entity geometry={{primitive: 'plane', width: 2, height: 2}} material="color: green" position={[0, 2, -1]} rotation={[-90, 0, 0]}/>
       </Scene>
     );
   }
