f2ec9c06dad442b81c89aaf181ae41cd8770011e
Pete Eichman <peichman14@gmail.com>
2017-03-15 23:54:15 -0400

    got rid of unnecessary files updated scene

--- src/VRScene.js
+++ src/VRScene.js
@@ -6,12 +6,9 @@ class VRScene extends React.Component {
   render () {
     return (
       <Scene>
-        <Entity geometry={{primitive: 'box', width: 2, height: 2}} material="color: red" position={[0, 0, -5]}/>
-        <Entity geometry={{primitive: 'sphere', radius: .25}} material="color: blue" position={[1, 2, -3]}/>
-        <Entity geometry={{primitive: 'plane', width: 2, height: 2}} material="color: green" position={[0, 0, -1]} rotation={[-90, 0, 0]}/>
-        <Entity geometry={{primitive: 'box', width: 2, height: 2}} material="color: red" position={[3, 4, 5]}/>
-        <Entity geometry={{primitive: 'sphere', radius: .25}} material="color: blue" position={[-1, -2, 3]}/>
-        <Entity geometry={{primitive: 'plane', width: 2, height: 2}} material="color: green" position={[0, 2, -1]} rotation={[-90, 0, 0]}/>
+        <Entity geometry={{primitive: 'box', width: 1, height: 1}} material="color: red" position={[0, .5, -4]}/>
+        <Entity geometry={{primitive: 'sphere', radius: .25}} material="color: blue" position={[0, 1.25, -4]}/>
+        <Entity geometry={{primitive: 'plane', width: 10, height: 10}} material="color: green" position={[0, 0, -5]} rotation={[-90, 0, 0]}/>
       </Scene>
     );
   }
