2f10fa9177a9e824aee20572ca7790a4f17eb499
Pete Eichman <peichman14@gmail.com>
2017-03-17 15:50:23 -0400

    added lights to spheres. added light to sky to counteract too much shadowing. completed cone.

--- src/VRScene.js
+++ src/VRScene.js
@@ -6,11 +6,14 @@ class VRScene extends React.Component {
   render () {
     return (
       <Scene>
-        <Entity geometry={{primitive: 'sphere', radius: 100}} material={{shader: 'flat', color: 'blue'}} scale="1 1 -1"/>
-        <Entity geometry={{primitive: 'box', width: 1, height: 1}} material="color: red" position={[0, .5, -4]}/>
-        <Entity geometry={{primitive: 'sphere', radius: .25}} material="color: yellow" position={[0, 1.25, -4]}/>
+        <Entity geometry={{primitive: 'sphere', radius: 100}} material={{shader: 'flat', color: '#bcbcbc'}} scale="1 1 -1" light={{type: 'ambient', color: "white", intensity:'4'}}/>
+        <Entity geometry={{primitive: 'sphere', radius: .25}} material="color: yellow" position={[-.5, 4, -4.5]} light={{type: 'point', color: "yellow", intensity:'4', distance: '50', decay: '2'}}/>
+        <Entity geometry={{primitive: 'sphere', radius: .25}} material="color: blue" position={[.5, 4, -3.5]} light={{type: 'point', color: "blue", intensity:'4', distance: '50', decay: '2'}}/>
+        <Entity geometry={{primitive: 'sphere', radius: .25}} material="color: red" position={[.5, 4, -4.5]} light={{type: 'point', color: "red", intensity:'4', distance: '50', decay: '2'}}/>
+        <Entity geometry={{primitive: 'sphere', radius: .25}} material="color: green" position={[-.5, 4, -3.5]} light={{type: 'point', color: "green", intensity:'4', distance: '50', decay: '2'}}/>
         <Entity geometry={{primitive: 'plane', width: 10, height: 10}} material="color: green" position={[0, 0, -5]} rotation={[-90, 0, 0]}/>
-        <Entity geometry={{primitive: 'cone', height: .5}} material="color: red" position={[0, 4, -4]}/>
+        <Entity geometry={{primitive: 'box', width: 1, height: 1}} material="color: red" position={[0, .5, -4]}/>
+        <Entity geometry={{primitive: 'cone', height: 1, radiusTop: 0, radiusBottom: 1}} material="color: black" position={[0, 2, -4]}/>
       </Scene>
     );
   }
